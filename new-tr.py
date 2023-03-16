bd60bd6d2d8c523b2b80ec8865b3fe3eaf5c23a5
kiwiyou <kiwiyou@kiwiyou.dev>
2023-03-16 13:46:47 +0900

    Add A+B for test

--- new-tr.py
+++ new-tr.py
@@ -4,6 +4,7 @@ from os import makedirs
 from json import dump, load
 parser = ArgumentParser(description="Generates a new translation document.")
 parser.add_argument('id')
+parser.add_argument('language')
 parser.add_argument('author')
 args = parser.parse_args()
 try:
@@ -15,17 +16,17 @@ try:
     text = text[text.index(begin) + len(begin):]
     text = text[:text.index(end)]
     directory = f'src/{args.id}'
-    filename = f'{directory}/{args.author}.html'
+    filename = f'{args.language}-{args.author}.html'
     makedirs(directory, exist_ok=True)
-    with open(filename, 'w+t') as f:
+    with open(f'{directory}/{filename}', 'w+t') as f:
         f.write(text.strip())
-    print(f'Generated {filename}.')
+    print(f'Generated {directory}/{filename}.')
     index = load(open('index', 'rt'))
     key = str(args.id)
     if key not in index:
         index[key] = []
-    index[key].append(args.author)
+    index[key].append(f'{args.language}-{args.author}')
     dump(index, open('index', 'wt'), indent=2, sort_keys=True)
-    print(f'Saved index.')
+    print('Saved index.')
 except Exception as e:
     print("Error:", e)
