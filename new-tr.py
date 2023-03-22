07e927a95028b6c537303810f7b920d1c441d991
thregium <35164579+thregium@users.noreply.github.com>
2023-03-23 02:23:20 +0900

    upd

--- new-tr.py
+++ new-tr.py
@@ -13,14 +13,17 @@ args = parser.parse_args()
 def find_tag_end(s):
     t = 0
     for i, c in enumerate(s):
-        if s[i] == '<' and s[i+1] == '/':
-            if t == 0:
-                return s[:i], s[i:]
-            t -= 1
-        elif s[i] == '<' and s[i+1] == 'i' and s[i+2] == 'm' and s[i+3] == 'g':
-            pass
-        elif s[i] == '<':
-            t += 1
+        if s[i] == '<':
+            if s[i+1] == '/':
+                if t == 0:
+                    return s[:i], s[i:]
+                t -= 1
+            elif s[i+1] == 'i' and s[i+2] == 'm' and s[i+3] == 'g':
+                pass
+            elif s[i+1] == 'b' and s[i+2] == 'r':
+                pass
+            else:
+                t += 1
     raise AssertionError("unreachable")
 
 
@@ -35,21 +38,22 @@ try:
     title_index = text.index('"problem_title"')
     title, _, text = text[title_index+16:].partition("</span>")
     match = None
-    sections = {"title": title}
+    sections = [{"id": "title", "content": title}]
     while (match := regex.search(text)):
         elem_id = match.group(1)
         content, text = find_tag_end(text[match.end(0):])
-        sections[elem_id] = re.sub(
-            meaningless, lambda match: f' {match.group(1)}', content)
+        sections.append({"id": elem_id, "content": re.sub(
+            meaningless, lambda match: f' {match.group(1)}', content)})
     directory = f'src/{args.id}'
     realname = f'{args.language}-{args.author}'
-    filename = f'{realname}.json'
+    filename = f'{realname}.html'
     os.makedirs(directory, exist_ok=True)
     if os.access(f'{directory}/{filename}', os.F_OK):
         print(repr(filename), 'already exists.')
         exit(1)
-    dump(sections, open(f'{directory}/{filename}',
-         'w+t', encoding='utf-8'), indent=2, sort_keys=False, ensure_ascii=False)
+    with open(f'{directory}/{filename}', 'wt') as f:
+        f.writelines(
+            f"<section id=\"{section['id']}\">{section['content']}</section>\n" for section in sections)
     print(f'Generated {directory}/{filename}.')
     index = load(open('index', 'rt', encoding='utf-8'))
     key = str(args.id)
